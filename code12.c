800c8f70106b676182a0302d60d1202b2cda5f55
SHIVANE99 <shivanepadhapc@gmail.com>
2024-11-16 12:08:39 +0530

    code12

---
+++ code12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    do{
+        printf("enter number :");
+        scanf("%d",&n);
+        if (n%2 !=0){
+            break;
+        }
+
+    }while(1);
+    printf("holaa amigoss");
+}
