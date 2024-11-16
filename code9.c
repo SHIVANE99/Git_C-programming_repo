7253566819f70c32dfaef6f49618f8618f8b6a54
SHIVANE99 <shivanepadhapc@gmail.com>
2024-11-16 11:13:26 +0530

    code9

---
+++ code9.c
@@ -0,0 +1,13 @@
+//print the number from 0 to n
+#include<stdio.h>
+int main(){
+   int n;
+   int i;
+   printf("enter number:");
+   scanf("%d",&n);
+   while(i<=n){
+       printf("%d\n",i);
+       i++;
+   }
+    
+}
