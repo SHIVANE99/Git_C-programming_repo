88196a25976b6fdee985252376cfc036e3ee7202
SHIVANE99 <shivanepadhapc@gmail.com>
2024-11-16 23:48:19 +0530

    code18

---
+++ code18.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int marks[3];
+    printf("enter maths marks :");
+    scanf("%d",&marks[0]);
+    printf("enter physics marks :");
+    scanf("%d",&marks[1]);
+    printf("enter chemistry marks :");
+    scanf("%d",&marks[2]);
+    printf("maths marks are :%d\n",marks[0]);
+    printf("physics marks are :%d\n",marks[1]);
+    printf("chemistry marks are :%d\n",marks[2]);
+
+
+}
