74fd60b3129a880e56886bc1bd70dad9eea0b1a4
SHIVANE99 <shivanepadhapc@gmail.com>
2024-12-06 13:58:55 +0530

    code 57

---
+++ code57.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a=65;
+    switch(a){
+        case 70 : 
+        printf("case 70");
+        break;
+        case 'A': 
+        printf("case A");
+        break;
+        default : 
+        printf("default case");
+        break;
+    }
+
+
+
+
+    return 0;
+}
