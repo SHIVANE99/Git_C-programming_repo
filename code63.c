b5f23875cb58252e07d06833eb28868ffc47d4db
SHIVANE99 <shivanepadhapc@gmail.com>
2024-12-09 11:05:08 +0530

    code63

---
+++ code63.c
@@ -0,0 +1,40 @@
+// armstrong number
+#include <stdio.h>
+int main()
+{
+    int n;
+    int count = 0;
+    int rem;
+    int mul = 1;
+    int value = 0;
+
+    printf("enter number");
+    scanf("%d", &n);
+    int q=n;
+    while (q != 0)
+    {
+        q = q / 10;
+        count++;
+    }
+    q = n;
+    while (q != 0)
+    {
+        rem = q % 10;
+        mul = 1;
+        for (int i = 0; i < count; i++)
+        {
+            mul = mul * rem;
+        }
+        value = value + mul;
+        q = q / 10;
+        mul = 1;
+    }
+    if (value == n)
+    {
+        printf("armstrong number");
+    }
+    else
+    {
+        printf("not an armstrong number");
+    }
+}
