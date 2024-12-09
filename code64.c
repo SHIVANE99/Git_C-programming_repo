4fbdb76ea116d9f635f645843c3b11b16a388c98
SHIVANE99 <shivanepadhapc@gmail.com>
2024-12-09 12:26:53 +0530

    code64

--- code64.c
+++ code64.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
-//pallindrome number
-// 121 ------------ 121
-int main (){
-    int n,c,rem;
-    int sum=0;
+// pallindrome number
+//  121 ------------ 121
+int main()
+{
+    int n, c, rem;
+    int sum = 0;
     printf("enter number :");
-    scanf("%d",&n);
-    c=n;
+    scanf("%d", &n);
+    c = n;
 
-    while(n!=0){
-        rem=n%10;
-        sum=rem+(sum*10);
-        n=n/10;
+    while (n != 0)
+    {
+        rem = n % 10;
+        sum = rem + (sum * 10);
+        n = n / 10;
     }
-    if(sum==c){
+    if (sum == c)
+    {
         printf("the number is pallindrome number");
     }
-    else{
+    else
+    {
         printf("not a pallindrome number");
     }
 
-
-
     return 0;
 }
-
