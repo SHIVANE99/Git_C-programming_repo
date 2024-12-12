4af81e856835bef19bdfb3073f44d705c6e8bd51
SHIVANE99 <shivanepadhapc@gmail.com>
2024-12-12 18:50:51 +0530

    code68

---
+++ code68.c
@@ -0,0 +1,28 @@
+// gcd eucledian
+#include <stdio.h>
+int main()
+{
+    int a, b, rem, numerator, denominator, gcd;
+    printf("enter 2 numbers :");
+    scanf("%d%d", &a, &b);
+    if (a > b)
+    {
+        numerator = a;
+        denominator = b;
+    }
+    else
+    {
+        numerator = b;
+        denominator = a;
+    }
+    rem = numerator % denominator;
+    while (rem != 0)
+    {
+        numerator = denominator;
+        denominator = rem;
+        rem = numerator % denominator;
+    }
+    gcd = denominator;
+    printf("gcd of %d & %d is : %d\n", a, b, denominator);
+    printf("lcm of %d & %d is : %d", a, b, (a * b) / gcd);
+}
