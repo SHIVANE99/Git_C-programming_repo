daebf57d7b9495f7737ed64c46a8c246cf7b47f1
SHIVANE99 <shivanepadhapc@gmail.com>
2024-12-13 22:42:31 +0530

    code70

---
+++ code70.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (){
+    int a=1;
+    int b=2;
+    switch(a)
+    {
+        case (1):
+        printf("case a");
+        break;
+        case(2):
+        printf("case b");
+        break;
+        case(3):
+        printf("case 3");
+        break;
+        default:
+        printf("default caseee...");
+        break;
+    }
+}
